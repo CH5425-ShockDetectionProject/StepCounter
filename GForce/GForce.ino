b398254ae2595f70224c9b6e6acc9dbcbba8ca51
Manu <mkp6781@gmail.com>
2023-04-12 15:25:07 +0530

    TCP connection established and Data sent

--- GForce/GForce.ino
+++ GForce/GForce.ino
@@ -10,6 +10,10 @@ Adafruit_MPU6050 mpu;
 const char *ssid = "manupi-connect";
 const char *password = "manupi123";
 
+// TCP
+int port = 8888;  //Port number
+WiFiServer server(port);
+
 const int MPU_addr=0x68;
 
 float accX=0, accY=0, accZ=0, gyroX = 0, gyroY = 0, gyroZ = 0;
@@ -41,16 +45,18 @@ void setup() {
   }
   digitalWrite(LED, HIGH); // Turn the LED off by making the voltage HIGH
 
+  // Finding MPU6050 and Initialising
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
   }
-
   Serial.println("MPU6050 Found!");
-  
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
 	mpu.setGyroRange(MPU6050_RANGE_500_DEG);
 	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   digitalWrite(LED, HIGH); // Turn the LED off by making the voltage HIGH
+
+  // Start the server
+  server.begin();
 }
 
 sensors_event_t a, g, t;
@@ -61,23 +67,33 @@ int num_digits_after_decimal=3;
 char strXAcc[20], strYAcc[20], strZAcc[20], strXgyro[20], strYgyro[20], strZgyro[20], data[60];
 
 void loop() {
-  mpu.getEvent(&a, &g, &t);
+  WiFiClient client = server.available();
+  if (client) {
+    if(client.connected()){
+      Serial.println("Client Connected");
+    }
+    while(client.connected()){      
+      // Fetch all sensor data from MPU6050
+      mpu.getEvent(&a, &g, &t);
+      accX = a.acceleration.x;
+      accY = a.acceleration.y;
+      accZ = a.acceleration.z;
+      gyroX = g.gyro.x;
+      gyroY = g.gyro.y;
+      gyroZ = g.gyro.z;
+      dtostrf(accX, min_width, num_digits_after_decimal, strXAcc);
+      dtostrf(accY, min_width, num_digits_after_decimal, strYAcc);
+      dtostrf(accZ, min_width, num_digits_after_decimal, strZAcc);
+      dtostrf(gyroX, min_width, num_digits_after_decimal, strXgyro);
+      dtostrf(gyroY, min_width, num_digits_after_decimal, strYgyro);
+      dtostrf(gyroZ, min_width, num_digits_after_decimal, strZgyro);
+      sprintf(data, "%s,%s,%s,%s,%s,%s\n", strXAcc, strYAcc, strZAcc, strXgyro, strYgyro, strZgyro);
 
-  accX = a.acceleration.x;
-  accY = a.acceleration.y;
-  accZ = a.acceleration.z;
-  gyroX = g.gyro.x;
-  gyroY = g.gyro.y;
-  gyroZ = g.gyro.z;
-  
-  dtostrf(accX, min_width, num_digits_after_decimal, strXAcc);
-  dtostrf(accY, min_width, num_digits_after_decimal, strYAcc);
-  dtostrf(accZ, min_width, num_digits_after_decimal, strZAcc);
-  dtostrf(gyroX, min_width, num_digits_after_decimal, strXgyro);
-  dtostrf(gyroY, min_width, num_digits_after_decimal, strYgyro);
-  dtostrf(gyroZ, min_width, num_digits_after_decimal, strZgyro);
-
-  sprintf(data, "#%s@%s$%s&%s!%s^%s", strXAcc, strYAcc, strZAcc, strXgyro, strYgyro, strZgyro);
-  // Serial.println(a.acceleration.x);
+      //Send Data to connected client
+      client.write(data);
+    }
+    client.stop();
+    Serial.println("Client disconnected");
+  }
   // Serial.println(data);
 }
