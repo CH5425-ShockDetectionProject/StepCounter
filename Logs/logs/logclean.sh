7f1cde4851b1055dab77c4f17aece3ca145bca36
Manu <mkp6781@gmail.com>
2023-04-30 13:30:50 +0530

    Script to clean log data

---
+++ Logs/logs/logclean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+while getopts f: option
+do 
+    case "${option}"
+        in
+        f)filename=${OPTARG};;
+    esac
+done
+
+echo "Filename: $filename"
+
+TOTAL=$(wc -l <$filename)
+echo $TOTAL
+sed -i "$TOTAL d" $filename
